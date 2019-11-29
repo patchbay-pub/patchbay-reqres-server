ad3534e5286ee80f20f0a35b46b08e37444ef664
Anders Pitman <tapitman11@gmail.com>
2019-11-29 13:34:40 -0700

    Rename to patchbay-reqres-server

--- cmd/patchbay-reqres-server/main.go
+++ cmd/patchbay-reqres-server/main.go
@@ -3,12 +3,13 @@ package main
 import (
         "log"
         "net/http"
+        pbreqres "github.com/anderspitman/patchbay-reqres-server"
 )
 
 func main() {
         log.Println("Starting up")
 
-        server := NewRequestResponseServer()
+        server := pbreqres.NewRequestResponseServer()
         err := http.ListenAndServe(":9001", http.HandlerFunc(server.Handle))
         if err != nil {
                 log.Fatal(err)
