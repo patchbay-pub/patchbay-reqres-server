d4ea5904960ea73b8e09d26681af730e7c16000f
Anders Pitman <tapitman11@gmail.com>
2019-12-02 19:01:45 -0700

    Move handler into method

--- request_response_server.go
+++ request_response_server.go
@@ -28,7 +28,8 @@ type PatchedReponse struct {
 }
 
 type RequestResponseServer struct {
-        Handle func(w http.ResponseWriter, r *http.Request)
+	channels map[string]chan PatchedRequest
+        mutex *sync.Mutex
 }
 
 
@@ -37,141 +38,140 @@ func NewRequestResponseServer() *RequestResponseServer {
 	channels := make(map[string]chan PatchedRequest)
 	mutex := &sync.Mutex{}
 
-        server := &RequestResponseServer{}
+        server := &RequestResponseServer{channels, mutex}
 
-	server.Handle = func(w http.ResponseWriter, r *http.Request) {
+        return server
+}
 
-		query := r.URL.Query()
+func (s *RequestResponseServer) Handle(w http.ResponseWriter, r *http.Request) {
+        query := r.URL.Query()
 
-                isResponder := query.Get("responder") == "true"
+        isResponder := query.Get("responder") == "true"
 
-                mutex.Lock()
-                _, ok := channels[r.URL.Path]
-                if !ok {
-                        channels[r.URL.Path] = make(chan PatchedRequest)
+        s.mutex.Lock()
+        _, ok := s.channels[r.URL.Path]
+        if !ok {
+                s.channels[r.URL.Path] = make(chan PatchedRequest)
+        }
+        channel := s.channels[r.URL.Path]
+        s.mutex.Unlock()
+
+        if isResponder {
+
+                log.Println("responder connection")
+
+                switchChannel := query.Get("switch") == "true"
+
+                // not all HTTP clients can read the response headers before sending the request body.
+                // switching splits the transaction across 2 requests, providing the responder 
+                // with a random channel for the second request, and connecting that to the original
+                // requester. These are referred to as "double clutch" requests. 
+                var switchChannelIdStr string
+                if switchChannel {
+                        switchChannelId, err := ioutil.ReadAll(r.Body)
+                        if err != nil {
+                                log.Fatal(err)
+                        }
+                        switchChannelIdStr = string(switchChannelId)
+
+                        if switchChannelIdStr == "" {
+                                w.WriteHeader(400)
+                                w.Write([]byte("No switching channel provided"))
+                                return
+                        }
+
+                        switchChannelIdStr = "/" + switchChannelIdStr
                 }
-                channel := channels[r.URL.Path]
-                mutex.Unlock()
 
-                if isResponder {
 
-                        log.Println("responder connection")
+                select {
+                case request := <-channel:
 
-                        switchChannel := query.Get("switch") == "true"
+
+                        for k, vList := range request.httpRequest.Header {
+                                for _, v := range vList {
+                                        w.Header().Add(RequestPrefix + k, v)
+                                }
+                        }
 
                         // not all HTTP clients can read the response headers before sending the request body.
                         // switching splits the transaction across 2 requests, providing the responder 
                         // with a random channel for the second request, and connecting that to the original
                         // requester. These are referred to as "double clutch" requests. 
-                        var switchChannelIdStr string
                         if switchChannel {
-                                switchChannelId, err := ioutil.ReadAll(r.Body)
-                                if err != nil {
-                                        log.Fatal(err)
-                                }
-                                switchChannelIdStr = string(switchChannelId)
 
-                                if switchChannelIdStr == "" {
-                                        w.WriteHeader(400)
-                                        w.Write([]byte("No switching channel provided"))
-                                        return
+                                io.Copy(w, request.httpRequest.Body)
+
+                                ch := make(chan PatchedRequest, 1)
+
+                                s.mutex.Lock()
+                                s.channels[switchChannelIdStr] = ch
+                                s.mutex.Unlock()
+
+                                ch <- request
+                        } else {
+
+                                doneSignal := make(chan struct{})
+                                response := PatchedReponse{responderRequest: r, doneSignal: doneSignal}
+
+                                contentType := mime.TypeByExtension(path.Ext(r.URL.Path))
+                                if contentType != "" {
+                                        response.responderRequest.Header.Set(ResponsePrefix + "Content-Type", contentType)
                                 }
 
-                                switchChannelIdStr = "/" + switchChannelIdStr
+                                io.Copy(w, request.httpRequest.Body)
+
+                                request.responseChan <- response
+
+                                <-doneSignal
                         }
+                case <-r.Context().Done():
+                        log.Println("responder canceled")
+                }
+        } else {
 
+                log.Println("requester connection")
 
-                        select {
-                        case request := <-channel:
+                responseChan := make(chan PatchedReponse)
+                request := PatchedRequest{httpRequest: r, responseChan: responseChan}
 
+                select {
+                case channel <- request:
 
-                                for k, vList := range request.httpRequest.Header {
+                        response := <-responseChan
+
+                        var status int
+                        for k, vList := range response.responderRequest.Header {
+                                if strings.HasPrefix(k, ResponsePrefix) {
+                                        // strip the prefix
+                                        headerName := k[len(ResponsePrefix):]
                                         for _, v := range vList {
-                                                w.Header().Add(RequestPrefix + k, v)
+                                                w.Header().Add(headerName, v)
+                                        }
+                                } else if strings.HasPrefix(k, "Pb-Status") {
+                                        var err error
+                                        status, err = strconv.Atoi(vList[0])
+                                        if err != nil {
+                                                log.Fatal(err)
                                         }
                                 }
-
-                                // not all HTTP clients can read the response headers before sending the request body.
-                                // switching splits the transaction across 2 requests, providing the responder 
-                                // with a random channel for the second request, and connecting that to the original
-                                // requester. These are referred to as "double clutch" requests. 
-                                if switchChannel {
-
-                                        io.Copy(w, request.httpRequest.Body)
-
-                                        ch := make(chan PatchedRequest, 1)
-
-                                        mutex.Lock()
-                                        channels[switchChannelIdStr] = ch
-                                        mutex.Unlock()
-
-                                        ch <- request
-                                } else {
-
-                                        doneSignal := make(chan struct{})
-                                        response := PatchedReponse{responderRequest: r, doneSignal: doneSignal}
-
-                                        contentType := mime.TypeByExtension(path.Ext(r.URL.Path))
-                                        if contentType != "" {
-                                                response.responderRequest.Header.Set(ResponsePrefix + "Content-Type", contentType)
-                                        }
-
-                                        io.Copy(w, request.httpRequest.Body)
-
-                                        request.responseChan <- response
-
-                                        <-doneSignal
-                                }
-                        case <-r.Context().Done():
-                                log.Println("responder canceled")
                         }
-                } else {
 
-                        log.Println("requester connection")
-
-                        responseChan := make(chan PatchedReponse)
-                        request := PatchedRequest{httpRequest: r, responseChan: responseChan}
-
-                        select {
-                        case channel <- request:
-
-                                response := <-responseChan
-
-                                var status int
-                                for k, vList := range response.responderRequest.Header {
-                                        if strings.HasPrefix(k, ResponsePrefix) {
-                                                // strip the prefix
-                                                headerName := k[len(ResponsePrefix):]
-                                                for _, v := range vList {
-                                                        w.Header().Add(headerName, v)
-                                                }
-                                        } else if strings.HasPrefix(k, "Pb-Status") {
-                                                var err error
-                                                status, err = strconv.Atoi(vList[0])
-                                                if err != nil {
-                                                        log.Fatal(err)
-                                                }
-                                        }
-                                }
-
-                                if status != 0 {
-                                        w.WriteHeader(status)
-                                }
-
-                                go func() {
-                                        <-r.Context().Done()
-                                        fmt.Println("requester canceled after connection")
-                                        response.responderRequest.Body.Close()
-                                }()
-
-                                io.Copy(w, response.responderRequest.Body)
-                                close(response.doneSignal)
-
-                        case <-r.Context().Done():
-                                log.Println("requester canceled before connection")
+                        if status != 0 {
+                                w.WriteHeader(status)
                         }
+
+                        go func() {
+                                <-r.Context().Done()
+                                fmt.Println("requester canceled after connection")
+                                response.responderRequest.Body.Close()
+                        }()
+
+                        io.Copy(w, response.responderRequest.Body)
+                        close(response.doneSignal)
+
+                case <-r.Context().Done():
+                        log.Println("requester canceled before connection")
                 }
         }
-
-        return server
 }
