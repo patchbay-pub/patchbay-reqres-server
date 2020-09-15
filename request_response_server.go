91d4b1e7bb0199720c76508bc898946b9098ab2d
Anders Pitman <tapitman11@gmail.com>
2020-09-15 16:29:41 -0600

    Don't fail on switch request cancel

--- request_response_server.go
+++ request_response_server.go
@@ -137,7 +137,9 @@ func (s *RequestResponseServer) Handle(w http.ResponseWriter, r *http.Request) {
                 if switchChannel {
                         switchChannelId, err := ioutil.ReadAll(r.Body)
                         if err != nil {
-                                log.Fatal("switch error", err)
+                                w.WriteHeader(400)
+                                w.Write([]byte("Switch request body canceled"))
+                                return
                         }
                         switchChannelIdStr = string(switchChannelId)
 
